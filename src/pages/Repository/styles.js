78a3a0dda4eafcaa77938891f082d78c111226b2
Françuel Soares <fs.francuelsoares@gmail.com>
2020-01-03 18:48:26 -0300

    0.14.0
    # New - Added Issues List

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -40,3 +40,61 @@ export const Owner = styled.header`
     max-width: 400px;
   }
 `;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 35px;
+      height: 35px;
+      border-radius: 50%;
+      border: solid 2px #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #eee;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          line-height: 1;
+          padding: 5px;
+          margin-left: 5px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 14px;
+        color: #666;
+      }
+    }
+  }
+`;
